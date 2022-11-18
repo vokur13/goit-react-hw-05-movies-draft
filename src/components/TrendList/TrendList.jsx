9d8dd120385359c3170c54d29f064522ed3d44de
Volodymyr Kurov <vokur13@gmail.com>
2022-11-18 10:19:28 +0200

    Update TrendList.jsx

--- src/components/TrendList/TrendList.jsx
+++ src/components/TrendList/TrendList.jsx
@@ -1,18 +1,6 @@
 import { Box } from 'components/Box';
 import { TrendListItem, NavItem } from './TrendList.styled';
 
-// export const TrendList = ({ data, onSelect }) => {
-//   return (
-//     <ul>
-//       {data.map(item => (
-//         <li key={item.id} onClick={() => onSelect(item.id)}>
-//           <a href={'movies'}>{item.title}</a>
-//         </li>
-//       ))}
-//     </ul>
-//   );
-// };
-
 export const TrendList = ({ data }) => {
   return (
     <Box m={2}>
